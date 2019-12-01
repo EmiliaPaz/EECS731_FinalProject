8f38bf98787dcad6d4fe6912ce59af3a6b89b129
EmiliaPaz <emilia.paz@ku.edu>
2019-12-01 15:50:29 -0600

    incidents clean data set

--- src/data/get_data.sh
+++ src/data/get_data.sh
@@ -1,2 +1,2 @@
-mkdir raw
 wget -nc -O assessor.csv https://data.sfgov.org/api/views/wv5m-vpq2/rows.csv?accessType=DOWNLOAD
+wget -nc -O incidents.csv https://data.sfgov.org/api/views/tmnf-yvry/rows.csv?accessType=DOWNLOAD&bom=true&format=true
