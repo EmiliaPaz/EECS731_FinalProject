3fcaff113ed6db6df1c1f140e560288abaf0ccd6
sandyr97 <rechs97@gmail.com>
2019-12-01 15:50:52 -0600

    added script for eviction dataset

--- src/data/get_data.sh
+++ src/data/get_data.sh
@@ -1,1 +1,2 @@
 wget -nc -O assessor.csv https://data.sfgov.org/api/views/wv5m-vpq2/rows.csv?accessType=DOWNLOAD
+wget -nc -O Eviction_Notices.csv https://data.sfgov.org/api/views/5cei-gny5/rows.csv?accessType=DOWNLOAD&bom=true&format=true
