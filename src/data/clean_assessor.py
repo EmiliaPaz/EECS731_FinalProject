498451eccfba875244c3a3cf5b3684e98a9b4695
Alex Shadley <shadleyalex@gmail.com>
2019-12-02 11:10:13 -0600

    adds starting point to anomaly detection

--- src/data/clean_assessor.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-assessor_data = pd.read_csv('assessor.csv')
-assessor_data = assessor_data.apply(lambda x: x.astype(str).str.lower())
-
-assessor_data = df.apply(lambda x: x.astype(str).str.lower())
